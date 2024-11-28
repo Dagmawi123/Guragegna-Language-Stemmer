eb2a30610e8cf30f0e7a9a0daaeb30ffe2282b46
barnabas <xobarneysolomon@gmail.com>
2024-11-28 13:40:10 +0300

    init

---
+++ Stemmer.cs
@@ -0,0 +1,12 @@
+namespace GuragegnaInfoRetrSys;
+
+public class Stemmer
+{
+    public List<string> DocPaths { get; set; }
+    
+    public void Tokenize(string parameter)
+    {
+        
+    }
+    
+}
